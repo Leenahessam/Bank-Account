efeb20b0bcdf4358448b2f2ed8afc86f22b8fc24
AYA0HASSAN <hassanaya520@gmail.com>
2022-05-11 18:53:06 +0200

    Adding some functions.

--- Main_Menu_Admin.cpp
+++ Main_Menu_Admin.cpp
@@ -7,7 +7,9 @@ void Main_Menu_Admin()
 {
     int choice;
     string existing_id;
+    string resever_id;
     int account_choice;
+    float cash;
     do
     {
         cout << "1- Create a new Account.\n"
@@ -35,15 +37,52 @@ void Main_Menu_Admin()
                 {
                 case 21:
                     // make transaction
+                    cout << "Enter Reciver Account ID.\n";
+                    cin >> resever_id;
+                    cout << "Enter Amount of Money To Be Transfared.\n";
+                    cin >> cash;
+                    bool result = makeTransaction(existing_id, resever_id, cash);
+                    if (result == true)
+                    {
+                        cout << "Transaction Done .\n";
+                    }
+                    else
+                    {
+                        cout << "Something Went Wronge Try Again.\n";
+                    }
                     break;
                 case 22:
                     // Change account status
                     break;
                 case 23:
                     // get cash
+                    cout << "Enter Amount of Money To Withdraw.\n";
+                    cin >> cash;
+                    float result = getCash(existing_id, cash);
+                    if (result != -1)
+                    {
+                        cout << "New Balance =  %f", result;
+                    }
+                    else
+                    {
+                        cout << "Something Went Wronge Try Again.\n";
+                    }
+
                     break;
                 case 24:
                     // deposite in a account
+                    cout << "Enter Amount of Money To Deposite.\n";
+                    cin >> cash;
+                    float result = depositeInAccount(existing_id, cash);
+                    if (result != -1)
+                    {
+                        cout << "New Balance =  %f", result;
+                    }
+                    else
+                    {
+                        cout << "Something Went Wronge Try Again.\n";
+                    }
+
                     break;
                 case 25:
                     // return to main menu
@@ -75,48 +114,41 @@ int findAccountIndex(string accountID)
 
 float getCash(string accountID, float cash)
 {
-    for (int i = 0; i < bankAccounts.size(); i++)
+    int idx = findAccountIndex(accountID);
+    if (bankAccounts[idx].accountID == accountID && bankAccounts[idx].status == 1)
     {
-        if (bankAccounts[i].accountID == accountID && bankAccounts[i].status == 1)
+        if (bankAccounts[idx].balance > cash)
         {
-            if (bankAccounts[i].balance > cash)
-            {
-                bankAccounts[i].balance -= cash;
-                return bankAccounts[i].balance;
-            }
+            bankAccounts[idx].balance -= cash;
+            return bankAccounts[idx].balance;
         }
     }
+
     return -1;
 }
 
 float depositeInAccount(string accountID, float cash)
 {
-    for (int i = 0; i < bankAccounts.size(); i++)
+    int idx = findAccountIndex(accountID);
+    if (bankAccounts[idx].status == 1)
     {
-        if (bankAccounts[i].accountID == accountID && bankAccounts[i].status == 1)
-        {
-            bankAccounts[i].balance += cash;
-            return bankAccounts[i].balance;
-        }
+        bankAccounts[idx].balance += cash;
+        return bankAccounts[idx].balance;
     }
     return -1;
 }
 
 bool makeTransaction(string senderAccountID, string reciverAccountID, float cash)
 {
-    for (int i = 0; i < bankAccounts.size(); i++)
+    int send_idx = findAccountIndex(senderAccountID);
+    int reseve_idx = findAccountIndex(reciverAccountID);
+    if (bankAccounts[send_idx].status == 1 && bankAccounts[send_idx].balance > cash)
     {
-        if (bankAccounts[i].senderAccountID == accountID && bankAccounts[i].status == 1 && bankAccounts[i].balance > cash)
+        if (bankAccounts[reseve_idx].status == 1)
         {
-            for (int j = 0; i < bankAccounts.size(); i++)
-            {
-                if (bankAccounts[i].senderAccountID == accountID && bankAccounts[i].status == 1)
-                {
-                    bankAccounts[i].balance -= cash;
-                    bankAccounts[j].balance += cash;
-                    return true;
-                }
-            }
+            bankAccounts[send_idx].balance -= cash;
+            bankAccounts[reseve_idx].balance += cash;
+            return true;
         }
     }
     return false;
